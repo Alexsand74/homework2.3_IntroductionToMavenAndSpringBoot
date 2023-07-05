e117fba57e3644a7aab4afd7312bd4354d23b863
Alexsand74 <1974alexsand@gmail.com>
2023-07-05 21:23:14 +0300

    Домашняя работа по теме 2.3. Введение в Maven и Spring Boot выполнена

--- src/main/java/homework/pro/sky/my/homework/СalculatorApplication.java
+++ src/main/java/homework/pro/sky/my/homework/СalculatorApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HomeworkSpringApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(HomeworkSpringApplication.class, args);
-	}
-
+public class СalculatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(СalculatorApplication.class,args);
+    }
 }
